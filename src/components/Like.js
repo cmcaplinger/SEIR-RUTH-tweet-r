e053e398f2ca7adbe5d9b6a37d7d30219a8ae70c
Alex <afbelardi@gmail.com>
2021-03-15 14:04:39 -0400

    created Like and Retweet components

---
+++ src/components/Like.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export default function Like(props){
+
+const [likes, addLikes] = useState(0);
+
+return (
+    <div>
+
+        
+    </div>
+
+
+
+    )
+
+
+
+
+
+
+}
+
+
+

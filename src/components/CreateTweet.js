245bfdad34bfbc8a34cea2591b9b3017f123fdf9
Alex <afbelardi@gmail.com>
2021-03-16 22:14:25 -0400

    added fetch from api to likes component

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-
-=======
 import React from 'react'
 import { useState } from 'react'
->>>>>>> origin/react
 // we can import a button component here if we like
 
 export default function CreateTweet() {
@@ -18,13 +13,8 @@ export default function CreateTweet() {
         // tweet schema here, based on the model Rosa wrote on the sketch
         // {
         //     user: "German",
-<<<<<<< HEAD
-        //     text: "dsfdsfdsfdfd",
-        //     mediaImage:       , 
-=======
             // text: "dsfdsfdsfdfd",
             // mediaImage:       , 
->>>>>>> origin/react
         //     comments: "sfdfdfdfdfd"
         // }
     }
--- src/components/Like.js
+++ src/components/Like.js
@@ -4,11 +4,13 @@ export default function Like(props){
 
 const [likes, addLikes] = useState(0);
 
+
 useEffect(() => {
     (async () => {
         try {
-            const res = await fetch('');
+            const res = await fetch('/seir-tweeter-api.herokuapp.com/tweets');
             const data = await res.json()
+            await addLikes(data.likes)
         } catch (error) {
             console.error(error)
         }
@@ -16,16 +18,32 @@ useEffect(() => {
 }, [])
 
 
+const updateLikes = async event => {
+    event.preventDefault();
+    try {
+        const response = await fetch(`/seir-tweeter-api.herokuapp.com/tweets/${props.match.params.id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                note: noteInput.current.value,
+                parkID: props.match.params.id
+            })
+        });
+        const data = await response.json();
+        setNotes([...notes, data]);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 return (
     <>
     <button onClick={event => addLikes(likes + 1)}>
         Like
     </button>
     <h4>{likes}</h4>
-<<<<<<< HEAD
-    
-=======
->>>>>>> origin/react
     </>
 )}
 

e5f46dd7da3fcc010c0bec289c1ba67c492a6a28
Caitlin Caplinger <caitlincaplinger@gmail.com>
2021-03-17 21:01:04 -0400

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,14 @@ import routes from '../router/routes';
 const NavBar = props => {
 	return (
 		<nav className="NavBar">
+			<div class="menu">
+				<div class="logo-wrapper">
+  				<div id="logo"></div>
+			</div>
+			<div class="menu-wrapper">
+				<div class="hamburger-menu"></div>
+			</div>
+			</div>
 			{props.routes.filter(item => item.key !== "Tweet").map(({ key, path }) => (
 				<Link key={key} to={path}>
 					{key}
